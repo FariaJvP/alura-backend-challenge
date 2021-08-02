af19c71fc8ad6eaba1ecaaa47e33f1cc0cddfa06
Joao Vitor <jvfaria.paz@gmail.com>
2021-08-02 14:00:10 -0300

    adiciona configurações e dependencias do maven no projeto

---
+++ videoapi/src/main/java/br/com/alura/videoapi/VideoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.videoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoApiApplication.class, args);
+	}
+
+}
---
+++ videoapi/src/test/java/br/com/alura/videoapi/VideoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.videoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

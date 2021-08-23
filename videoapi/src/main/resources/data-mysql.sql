2ba9c615b038ca84ddf63853a88419d0f0a1a105
Joao Vitor <jvfaria.paz@gmail.com>
2021-08-23 00:10:04 -0300

    adiciona arquivo de dados para o ambiente de desenvolvimento

---
+++ videoapi/src/main/resources/data-mysql.sql
@@ -0,0 +1,6 @@
+insert into aluravideoapi.video (id, titulo, descricao, url) values (1,'Spring Boot e API REST', 'descricao', 'https://cursos.alura.com.br/course/spring-boot-api-rest');
+insert into aluravideoapi.video (id, titulo, descricao, url) values (2,'Spring Data JPA', 'descricao', 'http://www.springdata.com.br');
+insert into aluravideoapi.video (id, titulo, descricao, url) values (3,'Clean Code', 'descricao', 'https://cursos.alura.com.br/course/solid-orientacao-objetos-java');
+insert into aluravideoapi.video (id, titulo, descricao, url) values (4,'Kotlin', 'descricao', 'https://cursos.alura.com.br/course/kotlin-heranca-polimorfismo-interface');
+insert into aluravideoapi.video (id, titulo, descricao, url) values (5,'Java e DDD', 'descricao', 'https://cursos.alura.com.br/course/java-domain-driven-design-conceitos');
+insert into aluravideoapi.video (id, titulo, descricao, url) values (6,'Kafka: Produtores, consumidores e streams', 'descricao', 'https://cursos.alura.com.br/course/kafka-introducao-a-streams-em-microservicos');
